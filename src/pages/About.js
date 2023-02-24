b3013f1574280a7beb3c31a654118bde2914e46c
Sri-Sriniavsan-webdev <srinivasan.webdev@gmail.com>
2023-02-24 14:53:55 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
+        About
+      </h1>
+      <p className="mt-4">
+        My name is Sri Srinivasan S.
+        <br />
+        I'm a Full Stack web developer based in Coimbatore, TamilNadu. I am a
+        professional and passionate programmer in my daily life. A quick learner
+        with a self-learning attitude. I love to learn and explore new
+        technologies and am Passionate about Problem Solving.
+        <br />
+        𝐌𝐲 𝐜𝐮𝐫𝐫𝐞𝐧𝐭 𝐬𝐭𝐚𝐜𝐤 𝐢𝐧𝐜𝐥𝐮𝐝𝐞𝐬 𝐇𝐓𝐌𝐋, 𝐂𝐒𝐒, 𝐁𝐨𝐨𝐭𝐬𝐭𝐫𝐚𝐩, 𝐉𝐚𝐯𝐚𝐒𝐜𝐫𝐢𝐩𝐭, 𝐑𝐞𝐚𝐜𝐭𝐉𝐬,
+        𝐍𝐨𝐝𝐞𝐉𝐬, 𝐄𝐱𝐩𝐫𝐞𝐬𝐬𝐉𝐬, 𝐌𝐨𝐧𝐠𝐨𝐃𝐁, 𝐌𝐲𝐒𝐐𝐋, 𝐆𝐈𝐓 (𝐌𝐄𝐑𝐍 𝐒𝐓𝐀𝐂𝐊).
+        <br />
+        <a href="https://srinidev.netlify.app/" target="_blank">
+          Click here to see Portfolio Site
+        </a>
+      </p>
+      <br />
+    </div>
+  );
+};
+
+export default About;
